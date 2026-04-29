c5b4b9473ae15696f7078508f7e256cdd0a3b375
Darren <darrendah7@gmail.com>
2026-04-29 20:28:38 +0700

    Menambah Navbar

--- resources/views/falkutas/add-falkutas.blade.php
+++ resources/views/falkutas/add-falkutas.blade.php
@@ -1,4 +1,10 @@
 <x-layout>
+    <!-- As a link -->
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
     <h1>Add Falkutas</h1>
     
 </x-layout>
